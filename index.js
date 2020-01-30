fd2e19c1bea72eb62f7b6818ac75c1b7a034eadf
yasmine-hj <yasminehajaji95@gmail.com>
2020-01-30 15:45:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+class Cat {
+    constructor(name,sex){
+        this.name = name
+        this.sex = sex
+    }
+
+speak(){
+    console.log(`${this.name} says meow!`)
+    return`${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name,sex){
+        this.name = name
+        this.sex = sex
+    }
+
+speak(){
+    console.log(`${this.name} says woof!`)
+    return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name,sex){
+        this.name = name
+        this.sex = sex
+    }
+    speak(){
+        if(this.sex === "male"){
+            console.log(`It's me! ${this.name}, the parrot!`);
+            return `It's me! ${this.name}, the parrot!`;
+        } else {
+            console.log(`${this.name}says squawk!`);
+            return `${this.name} says squawk!`;
+        }
+    }
+}
